478a82acfddada7b53366a05cd29df1912ac8b2a
unknown <rohansp00@gmail.com>
2020-11-10 14:37:52 +0530

    changed PORT

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const typeDefs = require("./graphql/typeDefs")
 const resolvers = require("./graphql/resolvers")
 
 const server = new ApolloServer({ typeDefs, resolvers, context: ({ req }) => ({ req, pubsub: new PubSub() }) })
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 mongoose
   .connect(MONGODBURL, { useNewUrlParser: true })
